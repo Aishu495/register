7ae95251c8cb2dfa8f599975f87a69fd2c6a83ad
Aishu495 <aishwaryad65@gmail.com>
2021-12-11 11:50:16 +0530

    first code

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-"mongodb+srv://edureka:1234@cluster0.qflcr.mongodb.net/zomato?retryWrites=true&w=majority"
+"mongodb+srv://edureka:1234@cluster0.nbutl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 );
